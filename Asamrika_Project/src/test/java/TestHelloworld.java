0d50b3869dc33281edcb4f1b04aed18640dad617
SamrikaSingh <samrika666@gmail.com>
2021-02-11 09:07:54 +0530

    First commit Uploading all the files

---
+++ Asamrika_Project/src/test/java/TestHelloworld.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class TestHelloworld {
+
+    @Test	
+	public static void main(String[] args) {
+
+		
+		System.out.println("hello java World");
+
+	}
+
+}
